5e27c150ea9b7c6399545456e99dfbcf208c3d6a
OpreaLucian <lucian.opreaa@gmail.com>
2015-11-17 22:18:36 +0200

    Third

---
+++ Problem3WithJavascript/ArrayFunctions.js
@@ -0,0 +1,30 @@
+﻿function VerifyIfHasDublicateValues(array)
+{
+    var i=0;
+    var j=i+1;
+
+    while (i < array.length - 1 && j <= array.length)
+    {
+        if (array[i]==array[j])
+        {
+            return true;
+        }
+
+        if (j==array.length-1)
+        {
+            i++;
+            j=i+1;
+        }
+        else
+            j++;
+    }
+
+    return false;
+}
+
+function GetResult(array)
+{
+    if(VerifyIfHasDublicateValues(array))
+        return "Yes";
+    return "No";
+}
---
+++ Problem3WithJavascript/Test/ArrayFunctionsTest.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="jasmine.js"/>
+/// <reference path="../ArrayFunctions.js"/>
+
+describe("VerifyIfArrayHasDublicateValues", function () {
+    it("WhenAreMixtValue_ReturnCorrectValue", function () {
+
+        var myArray = [1,-1,3,5];
+
+        var result = VerifyIfHasDublicateValues(myArray);
+
+        expect(result).toBeFalsy();
+    });
+
+    it("WhenContainsOnlyNegativeValues_ReturnCorrectValue", function () {
+
+        var myArray = [-1,-3,-55,-1];
+
+        var result = VerifyIfHasDublicateValues(myArray);
+
+        expect(result).toBeTruthy();
+    });
+
+    it("WhenContainsOnlyPositiveValues_ReturnCorrectValue", function () {
+
+        var myArray = [31,314,5,7,9];
+
+        var result = VerifyIfHasDublicateValues(myArray);
+
+        expect(result).toBeFalsy();
+    });
+
+    it("WhenContainsOneValue_ReturnCorrectValue", function () {
+
+        var myArray = [31];
+
+        var result = VerifyIfHasDublicateValues(myArray);
+
+        expect(result).toBeFalsy();
+    });
+
+    it("WhenContainsNoValues_ReturnCorrectValue", function () {
+
+        var myArray = [];
+
+        var result = VerifyIfHasDublicateValues(myArray);
+
+        expect(result).toBeFalsy();
+    });
+
+});
